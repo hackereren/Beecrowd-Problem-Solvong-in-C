fb59f5cd18c1ef28f29e4714ca304e79b3f5d927
ABIDUL HASAN AMIN <113846592+hackereren@users.noreply.github.com>
2024-10-05 13:22:47 +0600

    Add files via upload
    I try to solve some problem with C programing language in Beecrowd

---
+++ 1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+ //The problem name is Extremly basic
+    int A,B,X;
+    scanf("%d %d",&A,&B);
+    X=A+B;
+    printf("X = %d\n",X);
+    return 0;
+}
+
+
---
+++ 1002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    double R,A;
+    double n=3.14159;
+    scanf ("%1f",&R);
+    A= n * R*R;
+    printf ("A=%.4f\n",A);
+    return 0;
+}
